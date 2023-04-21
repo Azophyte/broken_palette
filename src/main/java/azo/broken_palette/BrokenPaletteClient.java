7a1f0a03b65acc821e898e0b9d9f54b67d68144b
Azo <Azophyte@gmail.com>
2023-04-21 15:49:39 +0100

    Initial commit

---
+++ src/main/java/azo/broken_palette/BrokenPaletteClient.java
@@ -0,0 +1,11 @@
+package azo.broken_palette;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BrokenPaletteClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/azo/broken_palette/mixin/BrokenPaletteMixin.java
@@ -0,0 +1,15 @@
+package azo.broken_palette.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class BrokenPaletteMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
